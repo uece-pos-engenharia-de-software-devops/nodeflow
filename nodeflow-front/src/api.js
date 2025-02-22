0d2b4273174c861a270f1a43bcc009a490b8a202
Marcos Eduardo <marcoseduardoss@gmail.com>
2025-02-22 20:39:17 -0300

    [Front] Incluão de opção para editar um nó

--- nodeflow-front/src/api.js
+++ nodeflow-front/src/api.js
@@ -3,9 +3,13 @@ import axios from "axios";
 const API_URL = "http://localhost:8080/api/nodes";
 
 export const getNodes = () => axios.get(API_URL);
+
 export const createNode = (node) => axios.post(API_URL, node);
+
 export const deleteNode = (id) => axios.delete(`${API_URL}/${id}`);
+
 export const connectNodes = (sourceId, targetId) =>
   axios.post(`${API_URL}/${sourceId}/connect/${targetId}`);
+
 export const disconnectNodes = (sourceId, targetId) =>
   axios.delete(`${API_URL}/${sourceId}/disconnect/${targetId}`);
